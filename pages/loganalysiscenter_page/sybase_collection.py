97afe99c66d10efc540505e4231a6cec8e901e58
van.yu@eoitek.com <yu123456>
2020-06-12 14:15:26 +0800

    0612

---
+++ pages/loganalysiscenter_page/sybase_collection.py
@@ -0,0 +1,49 @@
+import random
+import uuid
+
+from common.po_base import El
+from common.po_base import Page
+from common.page_action import PageAction
+from selenium.webdriver import Chrome
+from config import Driver_Path
+
+
+class LogHome(Page):
+    # 可以这么写单个元素
+    sidebar_element = El("侧边栏菜单", x="//span[text()='首页']")
+    key1 = El("ip/主机名", x="//input[@placeholder='输入主机名/IP']")
+    key2 = El("输入端口", x="//input[@placeholder='输入端口']")
+    key3 = El("输入数据库", x="//input[@placeholder='输入数据库名']")
+    key4 = El("输入用户名", x="//input[@placeholder='输入数据库用户名']")
+    key5 = El("输入密码", x="//input[@placeholder='输入数据库密码']")
+    key6 = El("任务名", x="//input[@placeholder='请输入任务名称']")
+    key7 = El("sql语句", css=".mr-10 > .el-input__inner")
+    key8 = El("采集周期", x="//input[contains(@placeholder,'输入5位')]")
+    # 也可以封装个函数 传侧边栏的名称
+    def click_sidebar_element(self, v):
+        self.click(x=f"//span[text()='{v}']")
+
+        return self
+
+    def sidebar_text(self,v):
+        text = self.sidebar_element.find_element_by_xpath(f"//span[text()='{v}']").text
+        return text
+
+    def click_element(self,v):
+         self.click(x=v)
+         return self
+
+    def click_css_element(self,v):
+         self.click(css=v)
+         return self
+
+    def return_name(self,v):
+        key = El("任务名称", x=f"//a[text()='{v}']")
+        return key
+
+    def random_str(self,num=6):
+        uln = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+        rs = random.sample(uln, num)  # 生成一个 指定位数的随机字符串
+        a = uuid.uuid1()  # 根据 时间戳生成 uuid , 保证全球唯一
+        b = ''.join(rs + str(a).split("-"))  # 生成将随机字符串 与 uuid拼接
+        return b  # 返回随机字符串
---
+++ testcase/test_loganalysiscenter/test_windows_file_collection.py
@@ -0,0 +1,44 @@
+import allure
+import pytest
+from pages.loganalysiscenter_page.windows_file_collection import LogHome
+from common.po_base import El
+from common.po_base import Page
+from pages.IndexPage import IndexPage
+from common.page_manage import pm
+from pages.LoginPage import LoginPage
+from time import sleep
+from pages.sensor_page.CreateEsPage import CreateEsPage
+@pytest.fixture(scope="module")
+def sign_in(login_as):
+    page=login_as("yuchengcheng5","yu123456")
+    page.click_platform("日志精析中心")
+    yield page
+@allure.feature("windows文件采集")
+class TestLogHome(object):
+    @allure.title("创建windows文件采集任务")
+
+    @allure.step("这是一个windows文件采集步骤")
+    def test_home(self,sign_in):
+        page = LogHome(sign_in)
+        page.click_sidebar_element('全局')
+        page.click_sidebar_element("数据接入")
+        page.switch_to_frame()
+        page.click_element("//a[text()='数据接入']")
+        page.click_element("//button[text()=' 创建']")
+        #page.click_element()
+        page.click_element("//li[text()='文件和目录']")
+        name = page.random_str()
+        page.key1.send_keys(name)
+        page.click_element("//a[text()='高级配置 ']")
+        page.click_element("//span[text()=' Windows ']")
+        page.click_element("//span[text()=' AIX ']")
+        page.click_element("//span[text()=' Linux ']")
+        page.click_element("//span[text()=' Windows ']")
+        page.key2.send_keys('192.168.31.219')
+        page.click_element("//tbody/tr[1]/td[1]/div/label/span/span")
+        page.key3.send_keys("C:\3.4-mave\mave\logs")
+        page.click_element("//button[text()='添加采集']")
+        page.click_element("//button[text()='保存']")
+
+        sleep(2)
+        assert page.return_name(name) != '',"任务不存在"
